d0c00c7e76f7d67a99ee2ca5ad7fcd0ed5269095
Thomas Stephens <tmsteph1290@gmail.com>
2023-11-21 18:59:40 +0000

    Update everything

---
+++ calendar-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is my Calendar
+        </p>
+        <h3>
+          Calendar
+        </h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
