dcac0bfff7b4c2bfaec3b12d52c88b531fa86395
Thomas Stephens <tmsteph1290@gmail.com>
2023-11-22 02:25:38 +0000

    Working calendar!

--- calendar-app/src/App.js
+++ calendar-app/src/App.js
@@ -1,28 +1,45 @@
+import React, { useState } from 'react';
 import './App.css';
-import Calendar from 'react-calendar'
+import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [dateEvents, setDateEvents] = useState({}); // Object to store events
+
+  const handleDateChange = (value) => {
+    setSelectedDate(value);
+  };
+
+  const handleNewEvent = () => {
+    const eventTitle = prompt('Enter Event Title');
+    if (eventTitle) {
+      const formattedDate = selectedDate.toISOString().split('T')[0];
+      const events = dateEvents[formattedDate] || [];
+      setDateEvents({
+        ...dateEvents,
+        [formattedDate]: [...events, { title: eventTitle }]
+      });
+    }
+  };
+
+  const formattedSelectedDate = selectedDate.toISOString().split('T')[0];
+  const events = dateEvents[formattedSelectedDate] || [];
 
   return (
     <div className="App">
       <header className="App-header">
-        
-        <p>
-          This is my Calendar
-        </p>
-        
-        <h1>
-          Calendar
-        </h1>
-        
-        <Calendar onChange={	(value, event) => alert('New date is: ' + value)}/>
-        
-         
+        <p>This is my Calendar</p>
+        <h1>Calendar</h1>
+        <Calendar onChange={handleDateChange} value={selectedDate} />
+        <button onClick={handleNewEvent}>New Event</button>
+        <div>
+          <h2>Events on {formattedSelectedDate}</h2>
+          {events.map((event, index) => (
+            <div key={index}>{event.title}</div>
+          ))}
+        </div>
       </header>
-
-
-      
     </div>
   );
 }
